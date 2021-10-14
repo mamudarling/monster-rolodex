68451f289d09971b3a5bd6f7206828c81e446265
raakesh <raakesh@divum.in>
2021-10-14 21:20:07 +0530

    adding files for git hub page

--- src/components/cardcomponent/card.component.jsx
+++ src/components/cardcomponent/card.component.jsx
@@ -5,7 +5,7 @@ const Card = (props) =>{
     return(
         <div className='card-container'>
             <img src={`https://robohash.org/${props.monster.id}?set=set2`} className='robo' alt='monster'/>
-         <h4 style={{textAlign:'center',color:'black'}}>{props.monster.name}</h4>
+         <h4 className='head'>{props.monster.name}</h4>
          <p style={{textAlign:'center',color:'black'}}>{props.monster.email}</p>
         </div>
     )
